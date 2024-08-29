e80a93b8aebc8f59bdfc07566b341814447e5456
Jeremy Fowler <jembyfowler@gmail.com>
2024-08-29 09:59:26 -0600

    drawing case files

--- app/controllers/CaseFilesController.js
+++ app/controllers/CaseFilesController.js
@@ -1,5 +1,16 @@
+import { AppState } from "../AppState.js";
+
 export class CaseFilesController {
   constructor() {
     console.log('Case Files controller is working');
+    this.drawCaseFiles()
+  }
+
+  drawCaseFiles() {
+    const caseFiles = AppState.caseFiles
+    let caseFileHTML = ''
+    caseFiles.forEach(caseFile => caseFileHTML += caseFile.listHTMLTemplate)
+    const caseFileElem = document.getElementById('case-files-list')
+    caseFileElem.innerHTML = caseFileHTML
   }
 }
